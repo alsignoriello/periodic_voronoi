9592c3356a4e14f4c2ded2b45f76537ba2094099
as2565 <alexandra.signoriello@yale.edu>
2016-03-22 11:08:28 -0400

    Running voronoi without periodic bounds correctly

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+
+HOME=/Users/Lexi/Documents/Ohern_2016/periodic_voronoi
+
+N=$1
+echo "N = "$N
+
+folder="test"
+# mkdir $folder
+cd $folder
+
+# run voronoi diagram without periodic boundaries
+# python $HOME/voronoi.py $N
+
+# plot voronoi diagram
+python $HOME/plot.py "voronoi.jpg"
+
+
+# run voronoi diagram with periodic boundaries
+
+
+# plot voronoi diagram
+
+
+
+
---
+++ voronoi.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import numpy as np
+from scipy.spatial import Voronoi
+import sys
+
+
+
+np.random.seed([1938420])
+
+
+N = int(sys.argv[1])
+
+# generate random points
+points = np.random.uniform(0,1, size=(N,2))
+
+# generate voronoi diagram
+vor = Voronoi(points)
+
+
+# write vertices
+np.savetxt("vertices.txt", vor.vertices)
+
+# write edges
+f = open("edges.txt", "w+")
+for edge in vor.ridge_vertices:
+	f.write("%d \t %d\n" % (edge[0], edge[1]))
+f.close()
+
+# write polygons
+f = open("polygons.txt", "w+")
+for region in vor.regions:
+	if len(region) != 0:
+		for index in region:
+			f.write("%d\t" % index)
+		f.write("\n")
+f.close()
+
